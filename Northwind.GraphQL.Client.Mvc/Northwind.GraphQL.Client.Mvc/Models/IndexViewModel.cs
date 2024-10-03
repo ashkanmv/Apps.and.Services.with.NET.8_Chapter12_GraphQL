e906a9b49d68d204a14df1ed49c708b26ec81b54
a.mirzaei <ashkanmv@gmail.com>
2024-10-04 01:08:53 +0330

    MVC project as a GraphQL client

---
+++ Northwind.GraphQL.Client.Mvc/Northwind.GraphQL.Client.Mvc/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Northwind.EntityModels;
+using System.Net;
+
+namespace Northwind.GraphQL.Client.Mvc.Models
+{
+    public class IndexViewModel
+    {
+        public HttpStatusCode Code { get; set; }
+        public string? RawResponseBody { get; set; }
+        public Product[]? Products { get; set; }
+        public Category[]? Categories { get; set; }
+        public Error[]? Errors { get; set; }
+    }
+}
